6a50501defc995c04296bd4c31b2aa3e38b95e25
Sheng Lundquist <sheng@delv.tech>
2024-07-09 15:30:35 -0700

    Fixing async agent0 example to work in manual mine mode

--- scripts/async_agent0_example.py
+++ scripts/async_agent0_example.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
 import asyncio
-import time
-from typing import TYPE_CHECKING
+import os
+from dataclasses import asdict
+from typing import TYPE_CHECKING, Sequence
 
 from agent0 import Chain, Hyperdrive, LocalChain, LocalHyperdrive
 from agent0.core.base.make_key import make_private_key
 from fixedpointmath import FixedPoint
+from web3.types import RPCEndpoint
 
 if TYPE_CHECKING:
     from agent0.core.hyperdrive.interactive.hyperdrive_agent import HyperdriveAgent
@@ -18,12 +20,12 @@ if __name__ == "__main__":
 
     # Launch a local chain object with hyperdrive deployed
 
-    server_chain = LocalChain(config=LocalChain.Config(db_port=1111))
+    server_chain = LocalChain(config=LocalChain.Config())
     server_pool = LocalHyperdrive(config=LocalHyperdrive.Config(), chain=server_chain)
 
     # Generate funded agents with private keys on the server
     # NOTE: this can also be done on the remote chain side
-    agent_pks = [make_private_key() for _ in range(2)]
+    agent_pks = [make_private_key() for _ in range(5)]
 
     server_agents = [
         server_chain.init_agent(
@@ -40,11 +42,23 @@ if __name__ == "__main__":
     _ = [agent.set_max_approval(pool=server_pool) for agent in server_agents]
 
     # Launch a client chain and pool connecting to the server
-    # TODO see if we can connect to the server database and have that db be in charge of
-    # syncing the db to the chain.
-    client_chain = Chain(rpc_uri=server_chain.rpc_uri, config=Chain.Config(db_port=2222))
+    # We connect the client chain to the server chain's db
+    postgres_config = asdict(server_chain.postgres_config)
+    # TODO `use_existing_postgres` reads from the environment (and an `.env` file), so we set it here.
+    # Ideally we can pass it in as a config
+    for k, v in postgres_config.items():
+        os.environ[k] = str(v)
+
+    client_chain = Chain(
+        rpc_uri=server_chain.rpc_uri,
+        config=Chain.Config(
+            use_existing_postgres=True,
+        ),
+    )
     client_pool = Hyperdrive(
-        chain=client_chain, hyperdrive_address=server_pool.hyperdrive_address, config=Hyperdrive.Config()
+        chain=client_chain,
+        hyperdrive_address=server_pool.hyperdrive_address,
+        config=Hyperdrive.Config(),
     )
 
     # Initialize the client agents
@@ -56,17 +70,18 @@ if __name__ == "__main__":
         for pk in agent_pks
     ]
 
+    ## After initialization, we set the chain to be manual mine mode
+    server_chain._web3.provider.make_request(method=RPCEndpoint("evm_setAutomine"), params=[False])
+
     ### Make async trades
 
-    # Need a async function wrapper to make the trade a coroutine
-    async def make_trade(agent: HyperdriveAgent) -> BaseHyperdriveEvent:
-        return agent.add_liquidity(base=FixedPoint(1_000))
-
     # Need async function definition for running the background tasks
-    async def run_trades(c_agents: list[HyperdriveAgent], s_chain: LocalChain) -> list[BaseHyperdriveEvent]:
-        # Make trades on the client side asynchronously
-        # Create strong references to prevent the agents from being garbage collected
-        background_tasks = [asyncio.create_task(make_trade(agent)) for agent in c_agents]
+    async def run_trades(c_agents: list[HyperdriveAgent], s_chain: LocalChain) -> Sequence[BaseHyperdriveEvent]:
+        # Make trades on the client side asynchronously via threads
+        # NOTE we need to do threads here because underlying functions use blocking waits
+        background_tasks = asyncio.gather(
+            *[asyncio.to_thread(agent.add_liquidity, base=FixedPoint(1_000)) for agent in c_agents]
+        )
 
         # Manually mine the block on the server side
         # NOTE We need to ensure the trades get submitted before we mine the block,
@@ -75,26 +90,29 @@ if __name__ == "__main__":
         # and background tasks gets stuck waiting for block to mine.)
         # TODO we achieve this by sleeping for now, there's probably a better way to do this,
         # e.g., ensuring the transactions are on the pending block.
-        time.sleep(1)
+        # NOTE this sleep must be asyncio sleep to give control to threads to submit the trades.
+        # `time.sleep` doesn't work here. In general, there's some strict order of operations here
+        # that gets complicated, likely due to the GIL.
+        await asyncio.sleep(1)
 
         # TODO we may want to expose an actual mine function,
         # but for now we advance time, which mines a block.
         # NOTE we can't create checkpoints here, because the server is running in
         # manual mining mode. Ensure we don't advance time more than a checkpoint duration.
-        # s_chain.mine()
-        s_chain.advance_time(time_delta=1800, create_checkpoints=False)
+        s_chain._web3.provider.make_request(method=RPCEndpoint("anvil_mine"), params=[])
+        # s_chain.advance_time(time_delta=1800, create_checkpoints=False)
 
         # Wait for all background tasks to finish
-        _ = [await task for task in background_tasks]
-
-        # Gather results
-        out = [task.result() for task in background_tasks]
-
-        # Remove references
-        background_tasks.clear()
+        out = await background_tasks
 
         return out
 
-    asyncio.run(run_trades(c_agents=client_agents, s_chain=server_chain))
+    out_events = asyncio.run(run_trades(c_agents=client_agents, s_chain=server_chain))
 
+    # View trades
+    # NOTE since the trades happened on the client object, and remote objects do lazy db updating,
+    # we need to either explicitly sync the server pool events, or reference events from the
+    # client pool (which automatically syncs events when called).
+    server_pool._run_blocking_data_pipeline()
+    events = server_pool.get_trade_events()
     pass
