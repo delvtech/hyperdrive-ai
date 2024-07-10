8f8937eaa51dddce8a3237caac6c45a1d9599cf6
Sheng Lundquist <sheng@delv.tech>
2024-07-10 15:01:59 -0700

    Shortcut import for gym envs

--- traiderdaive/__init__.py
+++ traiderdaive/__init__.py
@@ -2,6 +2,11 @@
 
 from gymnasium.envs.registration import register
 
+from .gym_environments.gym_full_hypedrive_env import FullHyperdriveEnv
+from .gym_environments.gym_simple_hyperdrive_env import SimpleHyperdriveEnv
+
+# TODO expose ray environments when ready
+
 # Register hyperdrive envs to gym
 register(
     id="traiderdaive/simple_hyperdrive_env",
