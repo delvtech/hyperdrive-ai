2fcd6a1116d747318b5e4dcf3716a74aab30d49d
Dylan <dpaiton@gmail.com>
2024-09-13 11:36:53 -0700

    docstring that explains the attack; updated action space

--- traiderdaive/ray_environments/attack_hyperdrive_env.py
+++ traiderdaive/ray_environments/attack_hyperdrive_env.py
@@ -23,8 +23,59 @@ warnings.filterwarnings("ignore")
 
 
 class AttackHyperdriveEnv(RayHyperdriveEnv):
-    """A simple hyperdrive environment that allows for 2 positions, long and short."""
+    """A hyperdrive environment catered to discovering the burrito attack vector.
 
+    Basic burrito attack steps per episode:
+      1. Manual agent LPs
+      2. RL agent opens a max short
+      3. RL agent LPs
+      4. RL agent opens a max long
+      5. Time elapses (x-axis amount in plot)
+      6. RL agent closes all positions
+      7. Manual agent closes all positions
+
+    An more difficult iterative version also exists, whereby time elapses by an
+    amount determined by the RL agent:
+      1. Manual agent LPs
+      2. for iteration in range(num_iterations):
+         OR
+         while(manual_agent_lp_balance > 0):
+           2.1. RL agent opens a max short
+           2.2. RL agent LPs
+           2.3. RL agent opens a max long
+           2.4. RL agent chooses not to trade for X steps
+               - the optimal X is determined by the pool config & agent budgets
+           2.5. RL agent closes all positions
+
+      3. Manual agent closes all positions (potentially receiving nothing)
+    """
+
+    def __init__(self, env_config) -> None:
+        # For this attack we only want one agent and zero random bots
+        assert self.env_config.num_random_bots == 0
+        assert self.env_config.num_random_hold_bots == 0
+        assert self.env_config.num_agents == 1
+        # The attack assumes they only hold 1 trade of each type
+        # Note that the bot _could_ learn this; we are enforcing it to help out
+        assert self.env_config.max_positions_per_type == 1
+        super().__init__(env_config)
+
+    def create_action_space(self) -> None:
+        """Returns the action space object."""
+        # Allowing the agent to propose 3 actions per step instead of 1
+        self._action_space_in_preferred_format = True
+        self.action_space = spaces.Dict(
+            {
+                agent_id: spaces.Box(
+                    low=-1e2,
+                    high=1e2,
+                    dtype=np.float64,
+                    shape=(3 * len(TradeTypes) * (self.env_config.max_positions_per_type + 2),),
+                )
+                for agent_id in self.agents
+            }
+        )
+        
     def _apply_action(self, agent_id: str, action: np.ndarray) -> list[bool]:
         """Execute the bot action on-chain.
 
@@ -64,6 +115,7 @@ class AttackHyperdriveEnv(RayHyperdriveEnv):
         # (2) Open long tokens
         # (4) Open short tokens
 
+<<<<<<<<<<<<<<  ✨ Codeium Command 🌟  >>>>>>>>>>>>>>>>
         # Get agent positions once so we don't hit the database too many times.
         # The downside of this is we could undershoot the max_positions_per_type
         # since any close actions this round will not be accounted for. This
@@ -79,97 +131,4 @@ class AttackHyperdriveEnv(RayHyperdriveEnv):
         trade_success[len(close_trade_success) : len(open_trade_success)] = open_trade_success
 
         return trade_success
-
-    def create_action_space(self) -> None:
-        """Create the action space object & assign it to self."""
-        # (longs, shorts) -> close_order_i(logit), new_order(logit), volume)
-        self._action_space_in_preferred_format = True
-        self.action_space = spaces.Dict(
-            {
-                agent_id: spaces.Box(
-                    low=-1e2,
-                    high=1e2,
-                    dtype=np.float64,
-                    shape=(len(TradeTypes) * (self.env_config.max_positions_per_type + 2),),
-                )
-                for agent_id in self.agents
-            }
-        )
-
-    def step(
-        self, action_dict: dict[str, np.ndarray]
-    ) -> tuple[dict[str, np.ndarray], dict[str, float], dict[str, bool], dict[str, bool], dict[str, Any]]:
-        """Takes a step in the the environment. Modified to showcase attack situation.
-
-        Arguments
-        ---------
-        action: ActType
-            An action provided by the agent to update the environment state
-
-        Returns
-        -------
-        tuple[np.ndarray, float, bool, bool, dict[str, Any]]
-            Contains the following
-
-            observation: ObsType
-                An element of the environment's observation_space.
-            reward: float
-                Reward for taking the action.
-            terminated: bool
-                Whether the agent reaches the terminal state, which can be positive or negative.
-                If true, user needs to call reset
-            truncated: bool
-                Whether the truncation condition outside the scope of the MDP is satisfied,
-                e.g., timelimit, or agent going out of bounds.
-                If true, user needs to call reset
-            info: dict[str, Any]
-                Contains auxiliary diagnostic information for debugging, learning, logging.
-        """
-        # TODO: Verify that env_config.episode length is working
-        # TODO: _apply_action() is per agent_id, but _get_observations() is for all agents. Make this consistent?
-        # TODO: Verify that truncated/terminated are being used correctly here. Do we need self.terminateds?
-        self.logger.info(f"\nStep {self._step_count} Time: {datetime.now().strftime('%I:%M:%S %p')}")
-        # Do actions and get truncated status for agents provided, and set the rest to True
-        self.interactive_hyperdrive.sync_database()
-
-        # Run apply action twice
-        for agent_id, action in action_dict.items():
-            _ = self._apply_action(agent_id, action)
-
-        for agent_id, action in action_dict.items():
-            _ = self._apply_action(agent_id, action)
-
-        # Run other bots
-        # Suppress logging here
-        for random_bot in self.random_bots:
-            try:
-                random_bot.execute_policy_action()
-            except BaseException as err:  # pylint: disable=broad-except
-                self.logger.warning(f"Failed to execute random bot: {repr(err)}")
-                # We ignore errors in random bots
-                continue
-
-        # We minimize time between bot making an action, so we advance time after actions have been made
-        # but before the observation
-        self.chain.advance_time(self.env_config.step_advance_time, create_checkpoints=True)
-
-        self.interactive_hyperdrive.sync_database()
-
-        observations = self._get_observations(agents=action_dict.keys())
-        info = self._get_info(agents=action_dict.keys())
-        step_rewards = self._calculate_rewards(agents=action_dict.keys())
-
-        episode_over = self._step_count >= self.env_config.episode_length - 1
-
-        truncateds = {agent_id: episode_over for agent_id in action_dict.keys()}
-        terminateds = {agent_id: False for agent_id in action_dict.keys()}
-
-        self.truncateds.update({agent_id for agent_id, truncated in truncateds.items() if truncated})
-        self.terminateds.update({agent_id for agent_id, terminated in terminateds.items() if terminated})
-
-        truncateds["__all__"] = len(self.truncateds) == len(self.agents)
-        terminateds["__all__"] = len(self.terminateds) == len(self.agents)
-
-        self._step_count += 1
-        # TODO when does the episode stop?
-        return observations, step_rewards, terminateds, truncateds, info
+<<<<<<<  c40afb40-be44-41b7-ba88-be13d033a012  >>>>>>>
