751aad137a1f81b7c4555da10bdea87429ce6fa3
Dylan <dpaiton@gmail.com>
2024-09-21 06:49:19 -0400

    value above initial lp reward

--- traiderdaive/ray_environments/rewards/__init__.py
+++ traiderdaive/ray_environments/rewards/__init__.py
@@ -2,5 +2,6 @@ from .delta_pnl import DeltaPnl
 from .realized_value import RealizedValue
 from .total_pnl import TotalPnl
 from .total_realized_value import TotalRealizedValue
+from .value_above_initial_lp import ValueAboveInitialLp
 
-__all__ = ["DeltaPnl", "RealizedValue", "TotalPnl", "TotalRealizedValue"]
+__all__ = ["DeltaPnl", "ValueAboveInitialLp", "RealizedValue", "TotalPnl", "TotalRealizedValue"]
