e58f0f7162c93c45c103d84f3752d462778d44c5
Will Shainin <wshainin@users.noreply.github.com>
2024-09-27 15:04:30 -0700

    Fixing scaling issue with FixedPoint account balance (#53)
    Fixing a disproportionate scaling of the reward value.

--- traiderdaive/ray_environments/rewards/value_above_initial_lp.py
+++ traiderdaive/ray_environments/rewards/value_above_initial_lp.py
@@ -6,6 +6,7 @@ from typing import Iterable
 
 import pandas as pd
 from agent0.ethpy.base import get_account_balance
+from fixedpointmath import FixedPoint
 
 from .base_reward import BaseReward
 
@@ -53,8 +54,9 @@ class ValueAboveInitialLp(BaseReward):
         # Get total value of agent address
         agent_positions = current_positions[current_positions["wallet_address"] == self.env.rl_agents[agent_id].address]
         total_realized_value = float(agent_positions["realized_value"].sum())
-        agent_eth_balance = get_account_balance(self.env.chain._web3, self.env.rl_agents[agent_id].address)
-        assert agent_eth_balance is not None  # type narrowing
+        scaled_value = get_account_balance(self.env.chain._web3, self.env.rl_agents[agent_id].address)
+        assert scaled_value is not None  # type narrowing
+        agent_eth_balance = float(FixedPoint(scaled_value=scaled_value))
         agent_total_value = total_realized_value + agent_eth_balance
         # Reward is the difference between these two total values
         return agent_total_value - lp_total_value
